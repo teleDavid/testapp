5d8d44b8500c53080bb2bd8b8c3ffba2ebeff11f
teleDavid <david@telecomstack.com>
2021-09-01 13:31:03 +0100

    testconnection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
+
+
+var AWS = require('aws-sdk/dist/aws-sdk-react-native');
+AWS.config.region = 'eu-west-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'eu-west-1:92b669d5-3f6f-42e4-ac66-937802f7c608',
+});
+
+
 function App() {
+
+  AWS.config.getCredentials(function(err) {
+    if (err) console.log(err.stack);
+    // credentials not loaded
+    else {
+      console.log("Access key:", AWS.config.credentials.accessKeyId);
+    }
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
